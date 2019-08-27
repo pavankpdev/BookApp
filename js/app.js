b6f3139bd5eaaa902f5fec69dc7d0041db26d309
pavanleton <pavankumarkpkp@gmail.com>
2019-08-27 18:57:30 +0530

    update some basic features

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-
 // Book class to hold the book object
-
+// This class 'Book' is assumed as local storage
 class Book {
   constructor(title, author, isbn, date) {
     this.title = title;
@@ -33,6 +32,8 @@ class UI {
       UI.addBookToList(books);
     });
   }
+
+  //method to add a book
   static addBookToList(books) {
     const list = document.querySelector(".table-body");
     const row = document.createElement("tr");
@@ -41,12 +42,53 @@ class UI {
       <td>${books.author}</td>
       <td>${books.isbn}</td>
       <td>${books.date}</td>
-      <td><i class="far fa-trash-alt text-danger fa-lg"></i></td>  
+      <td><i class="far fa-trash-alt text-danger fa-lg delete" id="trash"></i></td>  
       `;
     list.appendChild(row);
+  }
+
+  // method to delete a book
+  static deleteBook(element) {
+    if (element.classList.contains("delete")) {
+      element.parentNode.parentNode.remove();
+    }
+  }
+
+  static clearFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+    document.getElementById("release_date").value = "";
   }
 }
 
 // event listeners to load the table
 
-document.addEventListener("DOMContentLoaded",UI.displayBook());
+document.addEventListener("DOMContentLoaded", UI.displayBook());
+
+// Add a book from the UI
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+  const date = document.getElementById("release_date").value;
+
+  let books = new Book(title, author, isbn, date); // assuming that we're storing the book details in the local storage
+
+  // call a method from UI handler to display this values in the UI
+
+  UI.addBookToList(books);
+
+  // call a method to clear the input fields after submiting a book.
+
+  UI.clearFields();
+});
+
+// call a method to delete a book
+
+const trash = document.querySelector(".table-body");
+trash.addEventListener("click", element => {
+  UI.deleteBook(element.target);
+});
